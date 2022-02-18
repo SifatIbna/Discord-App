d87ba4ff6a27793bf93694c100712fc7ab0d309b
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-02-18 17:39:28 +0600

    established connection with mongodb

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-server.listen(PORT,()=>{
-    console.log(`Server is listening on ${PORT}`);
-});
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    server.listen(PORT,()=>{
+        console.log(`Server is listening on ${PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("database connection failed. Server not started");
+    console.log(err)
+})
