77715693319b1663552a64188a51cfd4180ef534
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-02-19 02:09:54 +0600

    added joi validtor for upcoming request

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const { valid } = require("joi");
 const router = express.Router();
+
+const Joi = require("joi");
+const validator = require("express-joi-validation").createValidator({});
+
+const registerSchema = Joi.object({
+    username: Joi.string().min(3).max(12).required(),
+    password: Joi.string().min(6).max(12).required(),
+    mail: Joi.string().email().required()
+});
+
+const loginSchema = Joi.object({
+    password: Joi.string().min(6).max(12).required(),
+    mail: Joi.string().email().required()
+});
 
 // Controllers
 const authControllers = require("../controllers/auth/authControllers");
 
-router.post("/register",authControllers.controllers.postRegister);
+router.post("/register",validator.body(registerSchema),authControllers.controllers.postRegister);
 
-router.post('/login',authControllers.controllers.postLogin);
+router.post('/login',validator.body(loginSchema),authControllers.controllers.postLogin);
 
 module.exports = router;
