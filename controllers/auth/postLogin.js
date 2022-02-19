c9cc080506d1d27f655811e5a3b946b7f48e057d
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-02-20 03:01:40 +0600

    added user model and postRegister and postLogin controllers

--- controllers/auth/postLogin.js
+++ controllers/auth/postLogin.js
@@ -1,5 +1,30 @@
+const User = require("../../models/user");
+const bcrypt  =require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const postLogin = async (req,res)=>{
-    res.send("Login Route")
+    try{
+        const {mail,password} = req.body;
+
+        const user = await User.findOne({mail:mail.toLowerCase()});
+
+        if(user && (await bcrypt.compare(password,user.password)))
+        {
+            const token = "JWT TOKEN";
+            return res.status(200).json({
+                userDetails: {
+                    mail:user.mail,
+                    token:token,
+                    username:user.username
+                }
+            })
+        }
+
+        return res.status(400).send("Inavalid Credentials.Please Try again!");
+    }
+    catch(err){
+        return res.status(500).send("Something went wrong!");
+    }
 }
 
 module.exports = postLogin;
--- controllers/auth/postRegister.js
+++ controllers/auth/postRegister.js
@@ -1,5 +1,48 @@
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const User = require("../../models/user");
+// const user = require("../../models/user");
+
 const postRegister = async (req,res)=>{
-    res.send("Register Route")
+    try {
+
+        const {username,mail,password} = req.body;
+
+        const userExists = await User.exists({
+            mail: mail.toLowerCase()
+        });
+
+        if(userExists) {
+            return res.status(400).send("E-mail already in use.");
+        }
+
+        // encrypt password
+        const encryptedPassword = await bcrypt.hash(password,10);
+
+        //creating the user and save the created model
+
+        const user = await User.create({
+            username,
+            mail: mail.toLowerCase(),
+            password: encryptedPassword
+        });
+
+        // created JWT token
+        const token = "JWT TOKEN";
+
+        res.status(201).json({
+            userDetails : {
+                mail: user.mail,
+                token:token,
+                username: user.username, 
+            }
+        })
+
+        // res.send("Register Route")
+    }
+    catch(err)
+    {
+        return res.status(500).send("Error occured. Please Try Again!")
+    }
 }
 
 module.exports = postRegister;
