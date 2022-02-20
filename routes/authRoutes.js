a4fc724c2ed9466d731ea6c72e1c6dbbcc5d7297
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-02-20 15:34:46 +0600

    added a test route to test the token is working or not

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const Joi = require("joi");
 const validator = require("express-joi-validation").createValidator({});
 
+const auth = require("../middlewares/auth")
+
 const registerSchema = Joi.object({
     username: Joi.string().min(3).max(12).required(),
     password: Joi.string().min(6).max(12).required(),
@@ -22,5 +24,11 @@ const authControllers = require("../controllers/auth/authControllers");
 router.post("/register",validator.body(registerSchema),authControllers.controllers.postRegister);
 
 router.post('/login',validator.body(loginSchema),authControllers.controllers.postLogin);
+
+//test route to verify if our middleware is working
+router.get('/test',auth,(req,res)=>{
+    res.send("request passed")
+});
+
 
 module.exports = router;
