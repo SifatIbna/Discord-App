0d4d658f706b84fb10ed1ebb9b8f7f6a82c24809
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-02-20 18:16:20 +0600

    removed a redundant file

--- discord-backend/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-    mail:{type:String, unique:true},
-    username:{type:String},
-    password:{type:String},
-});
-
-module.exports = mongoose.model("user",userSchema);
