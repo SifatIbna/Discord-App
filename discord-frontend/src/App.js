764aa8fd386be072ab6d6b1c36daabc0b1364ded
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-02-24 04:14:18 +0600

    downgraded the react router dom from 5 to 6 and created three navigation to different pages

--- discord-frontend/src/App.js
+++ discord-frontend/src/App.js
@@ -1,35 +1,38 @@
-import React from 'react';
-import {BrowserRouter as Router,Routes,Route, Navigate} from 'react-router-dom'
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import LoginPage from "./authPages/LoginPage/LoginPage";
+import RegisterPage from "./authPages/RegisterPage/RegisterPage";
+import Dashboard from "./Dashboard/Dashboard";
 
-import LoginPage from './authPages/LoginPage/LoginPage';
-import RegisterPage from './authPages/RegisterPage/RegisterPage';
-import Dashboard from './Dashboard/Dashboard';
+import "./App.css";
 
 function App() {
-  return <>
-    <Router>
-      <Routes>
-        <Route exact path="/login">
-          <LoginPage/>
-        </Route>
-
-        <Route exact path="/register">
-          <RegisterPage/>
-        </Route>
-
-        <Route exact path='/dashboard'>
-          <Dashboard/>
-        </Route>
-
-        <Route path='/'>
-          <Navigate to="/dashboard" />
-        </Route>
-
-      </Routes>
-    </Router>
-  </>
-  ;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/login">
+            <LoginPage />
+          </Route>
+          <Route exact path="/register">
+            <RegisterPage />
+          </Route>
+          <Route exact path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <Redirect to="/dashboard" />
+          </Route>
+        </Switch>
+      </Router>
+      
+    </>
+  );
 }
 
 export default App;
