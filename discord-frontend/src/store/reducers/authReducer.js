763000a608ff878e84b04d99836c99459d1cbef7
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-02-24 02:36:08 +0600

    added reducers and actions using redux thunk

---
+++ discord-frontend/src/store/reducers/authReducer.js
@@ -0,0 +1,17 @@
+const initState = {
+    userDetails: null
+};
+
+const reducer = (state = initState,action) => {
+    switch(action.type){
+        case "DUMMY":
+            return {
+                ...state,
+            };
+        
+        default:
+            return state;
+    }
+};
+
+export default reducer;
