50d45afba88e0c63b46a9add635165032e1ec003
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-02-26 23:14:50 +0600

    used the login page input component

--- discord-frontend/src/authPages/LoginPage/LoginPage.js
+++ discord-frontend/src/authPages/LoginPage/LoginPage.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import AuthBox from '../../shared/components/AuthBox'
+import LoginPageHeader from './LoginPageHeader'
+import LoginPageInputs from './LoginPageInputs';
 
 const LoginPage = () => {
+
+  const [mail,setMail] = useState("");
+  const [password,setPassword] = useState("");
+
   return (
-    <AuthBox>LoginPage</AuthBox>
+    <AuthBox>
+      <LoginPageHeader/>
+      <LoginPageInputs
+        mail={mail}
+        setMail = {setMail}
+        password = {password}
+        setPassword = {setPassword}
+      />
+
+    </AuthBox>
   )
 }
 
