bb7d12d00ef0ec5f6ffe13680fb14ae349b84786
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-02-26 23:12:57 +0600

    added the password and email inputs in the login page

---
+++ discord-frontend/src/authPages/LoginPage/LoginPageInputs.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import InputWithLabel from "../../shared/components/InputWithLabel"
+
+const LoginPageInputs = ({mail,setMail,password,setPassword}) => {
+  return (
+    <>
+        <InputWithLabel
+            value={mail}
+            setValue={setMail}
+            label="E-mail"
+            type="text"
+            placeholder="Enter e-mail address"
+        />
+
+        <InputWithLabel
+            value={password}
+            setValue={setPassword}
+            label="Password"
+            type="password"
+            placeholder="Enter Password"
+        />
+    </>
+  )
+}
+
+export default LoginPageInputs
