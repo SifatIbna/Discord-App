e35f07091708c7690631c5653951f148120c855a
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-02-27 23:54:47 +0600

    added the RedirectInfo component to the login page footer

--- discord-frontend/src/authPages/LoginPage/LoginPageFooter.js
+++ discord-frontend/src/authPages/LoginPage/LoginPageFooter.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import CustomPrimaryButton from '../../shared/components/CustomPrimaryButton'
 
+import RedirectInfo from '../../shared/components/RedirectInfo'
+import {useHistory} from "react-router-dom";
+
 const LoginPageFooter = ({handleLogin,isFormValid}) => {
+
+    const history = useHistory(); 
+    const handlePushToRegisterPage = ()=>{
+        history.push('/register')
+    };
+
   return (
     <>
         <CustomPrimaryButton
@@ -9,6 +18,12 @@ const LoginPageFooter = ({handleLogin,isFormValid}) => {
             additionalStyles={{marginTop: '30px'}}
             disabled={!isFormValid}
             onClick={handleLogin}
+        />
+        <RedirectInfo 
+            text="Need An Account?"
+            redirectText="Create an account"
+            additionaStyles={{marginTop:'5px'}}
+            redirectHandler={handlePushToRegisterPage}
         />
     </>
   )
